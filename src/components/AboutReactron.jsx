4f32f2c972e7f056637c1877a486c9688e3424d3
Seymour-creates <romelogilbert@yahoo.com>
2021-05-19 10:26:51 -0400

    splash update

---
+++ src/components/AboutReactron.jsx
@@ -0,0 +1,20 @@
+import react from 'react';
+
+
+const AboutReactron =()=> {
+
+  return(
+    <div>
+      <p>It</p>
+      <p>Just</p>
+      <p>Works</p>
+    </div>
+  )
+
+}
+
+
+
+
+
+export default AboutReactron;

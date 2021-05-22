00c173219befb49a9949942c7f2278fe44af2405
Seymour-creates <romelogilbert@yahoo.com>
2021-05-22 00:55:00 -0400

    team mem

--- src/components/team_members.js
+++ src/components/team_members.js
@@ -3,18 +3,19 @@
 // //import { Octokit } from "@octokit/core";
 const fetch = require('node-fetch');
 
-const getUser = async () => {
+const getUser = async  () => {
   const users = ['seymour-creates', 'odylic','kerriannercrawford','n8innate','SteeleCoale'];
-  const info = []
-  await users.forEach(async user => {
-    await fetch(`https://api.github.com/users/${user}`)
-    .then(async resp => await resp.json())
-    .then (async res => {console.log(res); return await info.push(res)})
+  const info = {}
+  users.forEach( user => {
+    fetch(`https://api.github.com/users/${user}`)
+    .then( resp => resp.json() )
+    .then( res => info[user] = res )
   })
   //console.log((info));
-  return  await (info);
+  return await info;
   
 }
+
 
 console.log(getUser());
 

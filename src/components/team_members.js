9e44217e9aff6fc8ebaf3aff7798076b1014defa
Jimmy <odylic.lin@gmail.com>
2021-05-26 17:57:23 -0400

    added pfp pics, styling, aboutreactron draft

--- src/components/team_members.js
+++ src/components/team_members.js
@@ -31,20 +31,48 @@
 
 import React from 'react';
 import TeamMemberComponent from './TeamMemberComponent';
+import Kerripfp from '../assets/Kerripfp.png';
+import Jimmypfp from '../assets/Jimmypfp.jpeg';
+import Loganpfp from '../assets/Loganpfp.jpeg';
+import Natepfp from '../assets/Natepfp.jpg';
+import Romelopfp from '../assets/Romelopfp.jpeg';
 
 export default function team_members() {
   return (
-    <div>
-      Team Members Header
+    <div className="TeamMembersWrapper">
+      <h2>Team Members Header</h2>
       <div className="TeamMembersDiv">
-          <TeamMemberComponent test={1} />
-          <TeamMemberComponent test={2} />
-          <TeamMemberComponent test={3} />
-          <TeamMemberComponent test={4} />
+        <TeamMemberComponent
+          test={1}
+          image={Kerripfp}
+          linkedin={'https://linkedin.com/in/kerriannercrawford'}
+          github={'https://github.com/kerriannercrawford'}
+        />
+        <TeamMemberComponent
+          test={2}
+          image={Jimmypfp}
+          linkedin={'https://www.linkedin.com/in/jimmylindpt/'}
+          github={'https://github.com/odylic'}
+        />
+        <TeamMemberComponent
+          test={3}
+          image={Loganpfp}
+          linkedin={''}
+          github={'https://github.com/SteeleCoale'}
+        />
+        <TeamMemberComponent
+          test={4}
+          image={Natepfp}
+          linkedin={''}
+          github={'https://github.com/n8innate'}
+        />
+        <TeamMemberComponent
+          test={5}
+          image={Romelopfp}
+          linkedin={''}
+          github={'https://github.com/Seymour-creates'}
+        />
       </div>
     </div>
   );
 }
-
-
-

17c0e14da6ffeca790116e1986013d2ad185f530
Jimmy <odylic.lin@gmail.com>
2021-05-26 14:27:11 -0400

    repush

---
+++ src/components/TeamMemberComponent.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+export default function TeamMemberComponent({test, image, github, linkedin}) {
+  return (
+    <div className="TeamMemberComponentDiv">
+        <image image={image}></image>
+        image
+        <br></br>
+        {test}
+        <br></br>
+        {linkedin}linkedin
+        <br></br>
+        {github}github
+    </div>
+  )
+}
+
+
+
+

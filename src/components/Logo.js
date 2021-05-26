368609e0902b72fcd2052e2b9f4d7d220f049cf1
Nathaniel A <ncarmstrong92@gmail.com>
2021-05-26 14:25:28 -0400

    Committing front page

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,41 +6,7 @@ export default function Logo() {
   return (
     <LogoStyledDiv>
       <main className="main">
-        <div className="Reactron-logo">
-          <div className="R-back">
-            <img src={logo} className="App-logo r-logo-back" alt="logo" />
-            <img src={logo} className="App-logo r-logo-back" alt="logo" />
-            <img src={logo} className="App-logo r-logo-back" alt="logo" />
-            <img src={logo} className="App-logo r-logo-back" alt="logo" />
-          </div>
-          <div className="R-front">
-            <div className="R-top-right">
-              <img
-                src={logo}
-                className="App-logo r-logo-top-right"
-                alt="logo"
-              />
-            </div>
-            <div className="R-bottom-right br-1">
-              <img
-                src={logo}
-                className="App-logo r-logo-bottom-right"
-                alt="logo"
-              />
-              <img
-                src={logo}
-                className="App-logo r-logo-bottom-right"
-                alt="logo"
-              />
-              <img
-                src={logo}
-                className="App-logo r-logo-bottom-right"
-                alt="logo"
-              />
-            </div>
-          </div>
-          <div className="eactron">EACTRON</div>
-        </div>
+        
       </main>
     </LogoStyledDiv>
   );

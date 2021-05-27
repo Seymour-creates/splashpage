07e603d6c5c73296d03ebe9c9c7c1a0656998a35
Seymour-creates <romelogilbert@yahoo.com>
2021-05-27 10:08:21 -0400

    to pull from origin

--- src/components/team_members.js
+++ src/components/team_members.js
@@ -1,31 +1,36 @@
-import styled from 'styled-components';
-import React from 'react';
+// import styled from 'styled-components';
+// import React from 'react';
+//import fetch from "node-fetch";
+const fetch = require('node-fetch');
 
-const getUser = async  () => {
+const getUser = async () => {
   const users = ['seymour-creates', 'odylic','kerriannercrawford','n8innate','SteeleCoale'];
   const info = {}
-  users.forEach( user => {
-    fetch(`https://api.github.com/users/${user}`)
-    .then( resp => resp.json() )
-    .then( res => info[user] = res )
-  })
-  //console.log((info));
-  return await info;
+  const user = []
+  for (let i = 0; i < users.length; i += 1) {
+    user.push( fetch(`https://api.github.com/users/${users[i]}`,{ auth : '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' }))
+    
+  }
+  Promise.all(user).then(resp => resp.forEach(resp => console.log(resp.json())))
+  
+  
+  //console.log(info);
+  //return info;
   
 }
 
+getUser();
 
-console.log(getUser());
 
-export default function team_members() {
-  return (
-    <div className="TeamMembersDiv">
-      <TeamMembersDiv>Team Members Div</TeamMembersDiv>
-    </div>
-  );
-}
+// export default function team_members() {
+//   return (
+//     <div className="TeamMembersDiv">
+//       <TeamMembersDiv>Team Members Div</TeamMembersDiv>
+//     </div>
+//   );
+// }
 
-export const TeamMembersDiv = styled.div`
-  border: 1px solid black;
-  height: 200px;
-`;
+// export const TeamMembersDiv = styled.div`
+//   border: 1px solid black;
+//   height: 200px;
+// `;

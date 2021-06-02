4a5b94efb391b116e6db97d683388cef4a771f2d
Jimmy <odylic.lin@gmail.com>
2021-06-02 16:43:33 -0400

    Made Buttons, paragraph and Logo smaller to screen size

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ export function NavBar() {
 
     return (
         <div className="NavBar">
-          <a className="website_link" href="https://reactron.io">Reactron.io</a>
+          <a className="website_link" href="https://reactron.io">Run Reactron</a>
           <a className="team_link" href="https://github.com/oslabs-beta/reactron">The Team</a>
         </div>
     )
